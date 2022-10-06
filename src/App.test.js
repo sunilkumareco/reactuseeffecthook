763b99e493ef6f738f020722affd8e4bbb947d01
Sunil Kumar <104414613+sunilkumareco@users.noreply.github.com>
2022-10-06 17:15:06 +0530

    Initial commits

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Current Cart Count', () => {
+test('Add to cart', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Current Cart Count/i);
+  const linkElement = screen.getByText(/Add to cart/i);
   expect(linkElement).toBeInTheDocument();
 });
