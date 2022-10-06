5c33b516067de58ba048396de340fca7186b6267
Sunil Kumar <104414613+sunilkumareco@users.noreply.github.com>
2022-10-06 16:57:44 +0530

    testbuild

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Current Cart Count', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Current Cart Count/i);
   expect(linkElement).toBeInTheDocument();
 });
