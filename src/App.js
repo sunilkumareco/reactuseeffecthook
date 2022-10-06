525b79ff1aa3a0678ae9287471e22f50e929a211
Sunil Kumar <104414613+sunilkumareco@users.noreply.github.com>
2022-10-06 17:15:24 +0530

    Morecomits

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 
-   // Declare state variables
-   const [count, setCount] = useState(0);
+  // Declare state variables
+  const [count, setCount] = useState(0);
+  const [product, setProduct] = useState("Eggs");
+  useEffect(() => {
+    console.log(`${product} will rule the world!`);
+  });
   return (
-    <div>
-        Current Cart Count: {count}
-            <div>
-            <button onClick={() => setCount(count - 1)}>Add to cart</button>
-            <button onClick={() => setCount(count + 1)}>Remove from cart</button>
-            </div>
-        </div>
+      <div>
+      Current {product}'s count: {count}
+          <div>
+              <button onClick={() => setCount(count + 1)}>Add to cart</button>
+              <button onClick={() => setCount(count - 1)}>Remove from cart</button>
+              Change Product:{" "}
+              <input type="text" onChange={(e) => setProduct(e.target.value)} />
+          </div>
+      </div>
   );
 }
 
